973d79b65eaa06a8f2dffc25eb1cdc787c2e71b6
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-10 13:16:09 +0200

    add render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import React, { useState } from 'react';
+import VideoSlider from './Slider/Slider';
+import VideoModal from './Modal/Modal';
+
+const App = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [, setCurrentVideoIndex] = useState(0);
+
+  const openVideoModal = index => {
+    setCurrentVideoIndex(index);
+    setModalIsOpen(true);
+  };
+
+  const closeVideoModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <VideoSlider openVideoModal={openVideoModal} />
+      <VideoModal
+        videoUrl={`https://player.vimeo.com/video/824804225`}
+        isOpen={modalIsOpen}
+        onRequestClose={closeVideoModal}
+      />
     </div>
   );
 };
+
+export default App;

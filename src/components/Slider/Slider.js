5425de05308da1c505f6948f6598a1d1dbdf0729
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-10 20:35:45 +0200

    change slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,29 +1,39 @@
+// VideoSlider.js
 import React from 'react';
-import Slider from 'react-slick';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
 
 const VideoSlider = ({ openVideoModal }) => {
   const videos = [
     'https://player.vimeo.com/video/824804225',
-    // Додайте інші посилання на відео за необхідністю
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
+    'https://player.vimeo.com/video/824804225',
   ];
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-  };
 
-    return (
-     <div className="slider-container">
-    <Slider {...settings}>
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={20}
+      slidesPerView={4}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={swiper => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
       {videos.map((video, index) => (
-        <div key={index}>
+        <SwiperSlide key={index}>
           <div
             className="video-thumbnail"
             onClick={() => openVideoModal(index)}
@@ -31,12 +41,12 @@ const VideoSlider = ({ openVideoModal }) => {
             <img
               src={`https://vumbnail.com/${video.split('/').pop()}.jpg`}
               alt={`Video ${index}`}
+              width={300}
             />
           </div>
-        </div>
+        </SwiperSlide>
       ))}
-            </Slider>
-            </div>
+    </Swiper>
   );
 };
 
